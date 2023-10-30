c2791fec626806ef3657293200a0b38d3855431c
lucastadeulc <lucastadeulc@gmail.com>
2023-10-30 16:18:29 -0300

    #FP-001 - Theme scaffold

---
+++ wp-content/plugins/pokemon/pokemon.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Pokemon
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     pokemon
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Pokemon
+ */
+
+// Your code starts here.
